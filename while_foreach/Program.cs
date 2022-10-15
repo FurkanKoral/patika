0e044ec80e3972e3387db1c39f5e113b8bfc8023
Furkan Koral <furkankoral41@gmail.com>
2022-10-15 15:50:57 +0300

    while_foreach dosyası eklendi

---
+++ while_foreach/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while
+            //1 den başlayarak konsoldan giren sayı dahil ortama hesaplayıp ekrana yazan program
+            Console.WriteLine("Bir sayi giriniz");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac<=sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+
+
+            //a dan z ye kadar tüm harfleri konsala yazdır
+            char character = 'a';
+            while(character<='z')
+            {
+                Console.Write(character);
+                character++;           
+            }
+
+            Console.WriteLine("**** Foreach Döngüsü ****");
+            string[] marka = {"Toyota","Mercedes","Audi"};
+
+            foreach (var arac in marka)
+            {
+                Console.WriteLine(arac);
+            }
+        
+        }
+    }
+}
