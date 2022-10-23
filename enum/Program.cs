4a5ac17e310a2ed30c521e10a14cb285074e7ec0
Furkan Koral <furkankoral41@gmail.com>
2022-10-23 18:36:08 +0300

    struct ve enum dosyası eklendi

---
+++ enum/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace enum_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            //enumlarda 
+            Console.WriteLine((int)Gunler.Cumartesi);
+            int sicaklik = 32;
+            if(sicaklik <= (int)HavaDurumu.Normal)
+                Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyelim");
+            else if(sicaklik >= (int)HavaDurumu.Sıcak)
+                Console.WriteLine("Dışarıya çıkmak için sıcak bir gün");
+            else if(sicaklik >= (int)HavaDurumu.Normal && sicaklik<(int)HavaDurumu.ÇokSıcak)
+                Console.WriteLine("Hadi dışarıya çıkalım");
+            
+
+
+
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma = 23,
+        Cumartesi,
+        Pazar
+    }
+
+
+    enum HavaDurumu
+    {
+        Soğuk = 5,
+        Normal = 20,
+        Sıcak = 25,
+        ÇokSıcak = 30
+    }
+}
